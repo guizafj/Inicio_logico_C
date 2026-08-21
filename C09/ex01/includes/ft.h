3c2b5506471720a07d4d5d091e7a77a8591c7a93
FJavier <contacto@dguiza.dev>
2026-08-21 22:12:15 +0200

    Implementación inicial de C09, pendiente ex02

---
+++ C09/ex01/includes/ft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fradiaz <fradiaz@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/21 21:12:21 by fradiaz           #+#    #+#             */
+/*   Updated: 2026/08/21 21:18:18 by fradiaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+
+# include <unistd.h>
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+void	ft_swap(int *a, int *b);
+# define FT_H
+
+#endif
