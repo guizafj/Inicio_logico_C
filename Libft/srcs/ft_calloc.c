18d735b12dbd31fc964b728177186e5c679c2117
FJavier <contacto@dguiza.dev>
2026-09-16 10:32:21 +0200

    Implementación ft_calloc Modificación ft_libft.h Mejora en la estructuración del proyecto

---
+++ Libft/srcs/ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fradiaz <fradiaz@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/15 13:14:51 by fradiaz           #+#    #+#             */
+/*   Updated: 2026/09/15 13:53:46 by fradiaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_libft.h"
+
+void	*calloc(size_t n, size_t size)
+{
+	void	*point;
+
+	if (n != 0 && (((size_t) - 1) / n) < size)
+		return (NULL);
+	point = malloc(n * size);
+	if (!point)
+		return (NULL);
+	ft_bzero(point, (n * size));
+	return (point);
+}
